08ab36122066b1bf523e0208d77d9e9a05263781
aureliebc <34154812+aureliebc@users.noreply.github.com>
2017-12-08 08:48:43 +0100

    Controleur gestion client

---
+++ gestionClient.php
@@ -0,0 +1,25 @@
+<?php
+
+class GestionClient extends CI_Controller
+{
+    public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this ->load -> database();
+		$this->miseAJour();
+	}
+
+	public function miseAJour()
+	{
+		$this->load->library('form_validation');
+		$this -> load -> model('connexion_Client_Modele');
+		
+		
+		}
+}
+
+?>
