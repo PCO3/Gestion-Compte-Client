7ab9bac8992ceacfaa15f7f6956c834ee6c8ef10
aureliebc <34154812+aureliebc@users.noreply.github.com>
2017-12-08 09:26:45 +0100

    Vue affichage Gestion Client

---
+++ FormulaireGestion.php
@@ -0,0 +1,42 @@
+
+<!Doctype>
+<html>
+
+	<head>
+
+		<title> BDD Client </title>
+		<h1> Mise à jour de la base de données Client </h1>
+	</head>
+
+	<body> 
+
+		<!-- il fatu afficher troi boutons ajouter, modifier, supprimer -->
+
+		<!---bouton Ajouter-->
+		<input type="button" value="ajouter un client">
+
+		</br>
+		</br>
+		</br>
+		
+		<TABLE BORDER="1"> 
+
+			<CAPTION> Comptes Client </CAPTION> 
+			<TR> 
+				<TH> Titre A1 </TH> 
+				<TH> Titre A2 </TH> 
+				<TH> Titre A3 </TH> 
+				<TH> Titre A4 </TH> 
+			</TR> 
+			<TR> 
+				<TH> Titre B1 </TH> 
+				<TD> Valeur B2 </TD> 
+				<TD> Valeur B3 </TD> 
+				<TD> Valeur B4 </TD> 
+			</TR> 
+		</TABLE> 
+
+	</body>
+
+</html>
+
