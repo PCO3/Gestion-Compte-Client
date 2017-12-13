e3f2c19265d12fa1ab2c9cbd9bea17feb15e8278
aureliebc <34154812+aureliebc@users.noreply.github.com>
2017-12-13 17:53:11 +0100

    Update connexion_Client_Modele.php
    Fonction pour les ajouts clients de l'administrateur

--- connexion_Client_Modele.php
+++ connexion_Client_Modele.php
@@ -16,32 +16,7 @@ class Connexion_Client_Modele extends CI_Model
 
 	}
 
-	public function modifyId($newPseudo, $oldPseudo)
-	{
-		//on vérifie que le nouveau pseudo n'est pas déjà utilisé
-		$resultat = $this->db->select('NomClient')
-							 ->from ('clientzodiac')
-							 ->where('NomClient', $newPseudo)
-							 ->get()
-							 ->result();
-
-		//si il n'existe pas
-		if(is_null($resultat))
-		{
-			//mettre à jour le nom du client 
-			$this->db->where('NomClient', $oldPseudo)
-					 ->update('NomClient', $newPseudo);
-			$message = 'mise à jour effectuée';
-		}
-		//Sinon on affiche un message d'erreur
-		else
-		{
-			$message= 'le nom client existe déja dans la base de données';
-		}
-
-		return $message;
-	}
-
+	
 	public function modifyPassword($oldPassword, $newPassword, $pseudo)
 	{
 		$this->db->where('NomClient', $pseudo)
@@ -53,21 +28,92 @@ class Connexion_Client_Modele extends CI_Model
 
 	//public function deleteUser($pseudo)
 		
-	public function insertClient($pseudo, $Password, $email) //insérer client
+public function insertClient($pseudo, $Password, $email) //insérer client
 	{
-		$this->db->set('NomClient', $pseudo);
-		$this->db->set('Password', $Password);
-		$this->db->set('email', $email);
-		$this->db->insert($this->clientzodiac);
+		$dbName= 'clientzodiac';
+		$data= array(
+				'NomClient' => $pseudo,
+				'Password' => $Password,
+				'email' => $email
+		);
+		$this->db->insert($dbName,$data);
 		$message='Client inséré';
 		return $message;
 	}
-	public function deletClient($pseudo)
+
+
+	public function deleteClient($pseudo)
 	{
 		$this->db->where('NomClient',$pseudo);
 		$this->db->delete('clientzodiac');
 		$message='Client supprimé';
 		return $message;
+	}
+
+	//fonction pour récuperer les identifiants de tous les clients sous forme de tableaux
+
+	public function allId()
+	{
+		$ids = $this->db->select('NomClient')
+						->from('clientzodiac')
+						->get()
+						->result();
+
+		//on recupere tous les ids sous forme de stdClass pour les mettre en string dans un tableau
+		foreach ($ids as $id ) {
+
+			$Id=(array) $id;
+			$tabIds[]=$Id['NomClient'];
+			
+		}
+		//on récupere le tableau d'ids
+		return $tabIds;
+
+	}
+
+	//fonction pour récuperer les email de tous les clients
+
+	public function allEmail()
+	{
+		$emails = $this->db->select('email')
+						   ->from('clientzodiac')
+						   ->get()
+						   ->result();
+
+		//on recupere tous les emails sous forme de stdClass pour les mettre en string dans un tableau
+		foreach ($emails as $email ) {
+
+			$Email=(array) $email;
+			$tabEmail[]=$Email['email'];
+			
+		}
+		//on récupere le tableau d'emails
+		return $tabEmail;
+
+	}
+
+	public function uniqueId($newId, $tabId)
+	{
+		$unique = TRUE;
+		$tabIds = $tabId;
+		for($i=0; $i<count($tabIds); ++$i){
+			if($tabIds[$i]==$newId){
+				$unique=FALSE;
+			}
+		}
+		return $unique;
+	}
+
+	public function uniqueEmail($newEmail, $tabEmail)
+	{
+		$unique = TRUE;
+		$tabEmails = $tabEmail;
+		for($i=0; $i<count($tabEmails); ++$i){
+			if($tabEmails[$i]==$newEmail){
+				$unique=FALSE;
+			}
+		}
+		return $unique;
 	}
 	
 	public function affichageclients() //afficher les clients
