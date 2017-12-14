ae9e09f322b9bf48bf02c0a3baaedf10450f215d
aureliebc <34154812+aureliebc@users.noreply.github.com>
2017-12-14 11:50:52 +0100

    Add files via upload

---
+++ Base_HTML/Header.php
@@ -0,0 +1,24 @@
+<?php $this->load->helper('url'); ?>
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/test.css">
+        <title>ZODIAC aerospace</title>
+
+    </head>
+
+    <header>
+
+        <div id="logo">
+            <img src="<?php echo base_url(); ?>css/Zodiac.png" alt=logoheader/>
+        </div>
+
+    </header>
+    
+</html>
+
